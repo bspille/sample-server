6ff2e8e5b157c5442069406fb08cdf9738eed19f
Benjamin Spille <Benjamin Spille>
2017-04-11 19:43:03 -0400

    cleaned up server code

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 var http = require('http');
 // where the server listens
-var PORT1 = 7000;
+var port1 = 7000;
+var port2 = 7050;
+// call these ports in the browser localhost:7000
 // handles requests
-function handleRequest(request, response){
+function handleRequest1(request, response){
 	// this will be triggered when the user goes to the port
 	response.end('it works! path hit; ' + request.url + '\n'+ 'you are dilligent');
 }
-// create server
-var server = http.createServer(handleRequest);
-// start server
-server.listen(PORT1, function(){
-	console.log('server is listening on port: ' + PORT1);
-});
-var http = require('http');
-// where the server listens
-var PORT2 = 7050;
-// handles requests
-function handleRequest(request, response){
+function handleRequest2(request, response){
 	// this will be triggered when the user goes to the port
 	response.end('it works! path hit; ' + request.url + '\n' + 'you have a bad attitude go back to the grind stone');
 }
 // create server
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest1);
+var server = http.createServer(handleRequest2);
 // start server
-server.listen(PORT2, function(){
-	console.log('server is listening on port: ' + PORT2);
+server.listen(port1, function(){
+	console.log('server is listening on port: ' + port1);
+});
+server.listen(port2, function(){
+	console.log('server is listening on ports: ' + port2);
 });
